b9608e5512c2c749786b5a8f9566768d1fc5b276
Pavlo Hurkovskyi <>
2025-10-29 20:04:14 +0100

    removed panels

--- OpenfortAuthorization/OpenfortAuthorization/HomeView.swift
+++ OpenfortAuthorization/OpenfortAuthorization/HomeView.swift
@@ -69,8 +69,8 @@ struct HomeView: View {
                                 }
                                 .tint(.red)
                                 VStack(spacing: 20) {
-                                    // Account actions
-                                    AccountActionsView(handleSetMessage: viewModel.handleSetMessage)
+                                   /* // Account actions
+                                    AccountActionsView(handleSetMessage: viewModel.handleSetMessage)*/
                                     
                                     // Signatures
                                     SignaturesPanelView(handleSetMessage: viewModel.handleSetMessage, embeddedState: viewModel.state)
@@ -80,12 +80,12 @@ struct HomeView: View {
                                     
                                     // Embedded wallet
                                     EmbeddedWalletPanelView(handleSetMessage: viewModel.handleSetMessage, viewModel: EmbeddedWalletPanelViewModel(embeddedState: viewModel.state, embeddedAccount: viewModel.embeddedAccount))
-                                    
+                                   /*
                                     // Wallet Connect
                                     WalletConnectPanelView(viewModel: WalletConnectPanelViewModel())
                                     
                                     // Funding
-                                    FundingPanelView(handleSetMessage: viewModel.handleSetMessage)
+                                    FundingPanelView(handleSetMessage: viewModel.handleSetMessage)*/
                                 }
                                 .padding(.top, 12)
                             }
